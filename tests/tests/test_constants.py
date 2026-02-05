40afc8da9b176f4abb1f8e92a975596fcfbe9f59
Ilver Villasmil <ilvervillasmil@gmail.com>
2026-02-05 04:20:06 -0500

    Delete tests/tests/test_constants.py

--- tests/tests/test_constants.py
+++
@@ -1,8 +0,0 @@
-# File: tests/test_constants.py
-from formulas.constants import PHI, ALPHA, BETA, S_REF
-
-def test_constants():
-    assert round(PHI, 5) == 1.61803, "PHI constant is incorrect!"
-    assert round(ALPHA, 5) == 0.96296, "ALPHA constant is incorrect!"
-    assert round(BETA, 5) == 0.03704, "BETA constant is incorrect!"
-    assert round(S_REF, 5) == 0.86526, "S_REF constant is incorrect!"
